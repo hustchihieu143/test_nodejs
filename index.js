91a9c64e36e275f6371d282d9fe166addc8421bb
hustchihieu143 <chihieusky@gmail.com>
2023-05-30 23:45:50 +0700

    delete

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/health', (req, res) => {
-  res.send('Hello World healthcheck!')
-})
-
-app.listen(3000, () => {
-  console.log(`Example app listening on port 3000`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
