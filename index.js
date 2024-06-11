9bb53823eb20a330f70cb7126546fa7b28bcdd1b
hieupc <hieu.pc@cmctelecom.vn>
2024-06-11 15:44:03 +0700

    al

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port test 8 ${port}`)
+  console.log(`Example app listening on port test 9 ${port}`)
 })
