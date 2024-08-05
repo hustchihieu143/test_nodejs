389afe9ec0b35b7881d0e3a8ec19699da298415b
iamhieupc <chihieusky@gmail.com>
2024-08-05 14:23:05 +0700

    fix

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! test5')
+  res.send('Hello World! test6')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port test 9 ${port}`)
-})
+}
