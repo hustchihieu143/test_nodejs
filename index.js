2be6a56013db711f0d8523e198bf6ef4497aed9f
iamhieupc <chihieusky@gmail.com>
2024-08-14 13:52:08 +0700

    a

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World! test1')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port test 10 ${port}`)
-})
+console.log("hello")
