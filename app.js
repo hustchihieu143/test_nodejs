fe62cb592509446d91babe4efe94a6d72422ea94
manh.pd <manh.pd@cmctelecom.vn>
2024-08-19 10:18:56 +0700

    fix

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // app.js
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World! 2');
+app.get("/", (req, res) => {
+  res.send("Hello, World! 3333");
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
 
 // utils.js
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World! test6')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World! test 10");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port test 10 ${port}`)
-})
+  console.log(`Example app listening on port test 10 ${port}`);
+});
