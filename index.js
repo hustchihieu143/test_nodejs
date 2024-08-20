7d19ed44b47e80a4d7a2ca6a7fb6b6f803ee0c39
iamhieupc <chihieusky@gmail.com>
2024-08-20 14:20:05 +0700

    fix

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello11");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World! test 11");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port test 10 ${port}`);
+});
