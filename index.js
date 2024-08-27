f796db53139a988e073eefeb9c34a08e1e3b1e20
Hieu Phan <72189639+iamhieupc@users.noreply.github.com>
2024-08-27 09:25:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! test1 111 ");
+  res.send("Hello World! test1 1 ");
 });
 
 app.listen(port, () => {
