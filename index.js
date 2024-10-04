f578de43b7a2a6dd02098085ef7a6924cd6e729f
ManhPhamGitHub <90790565+ManhPhamGitHub@users.noreply.github.com>
2024-10-04 16:45:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port test 123 ${port}`);
+  console.log(`Example app listening on port test eqwq ${port}`);
 });
