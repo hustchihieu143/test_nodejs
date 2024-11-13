41ba832bcf16be3f1f811b2504772fec56c0947c
Hieu Phan <72189639+iamhieupc@users.noreply.github.com>
2024-11-13 14:10:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port 3000 ${port}`);
+  console.log(`Example app listening on port 3001 ${port}`);
 });
